2fa47fea84a769da91a9c5aec35528bccaea7195
Roger Pereira <116629830+RogerSouza1@users.noreply.github.com>
2024-08-22 20:53:04 -0300

    initial commit

---
+++ vinicola/src/test/java/br/com/vinicola/VinicolaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vinicola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinicolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
