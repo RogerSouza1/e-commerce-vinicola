e2964a2bc3b2da445876c21ea4e8c7eadfd944b9
Roger Pereira <116629830+RogerSouza1@users.noreply.github.com>
2024-08-26 21:55:50 -0300

    Add files via upload

---
+++ vinicola/src/main/java/com/orange/vinicola/VinicolaApplication.java
@@ -0,0 +1,13 @@
+package com.orange.vinicola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinicolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinicolaApplication.class, args);
+	}
+
+}
---
+++ vinicola/src/test/java/com/orange/vinicola/VinicolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.vinicola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinicolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
