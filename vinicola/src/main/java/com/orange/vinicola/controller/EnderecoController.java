efed51af31c9b26fb70608ea1d55413304714021
Roger Pereira <116629830+RogerSouza1@users.noreply.github.com>
2024-10-21 17:50:35 -0300

    fix: Recommit

--- vinicola/src/main/java/com/orange/vinicola/controller/EnderecoController.java
+++ vinicola/src/main/java/com/orange/vinicola/controller/EnderecoController.java
@@ -76,14 +76,16 @@ public class EnderecoController {
     }
 
     @GetMapping("/cadastrar")
-    public String cadastrarEndereco(Model model) {
+    public String cadastrarEndereco(@RequestParam(required = false) String redirect, Model model) {
         model.addAttribute("endereco", new Endereco());
+        model.addAttribute("redirect", redirect);
         return "registrar-endereco";
     }
 
     @PostMapping("/cadastrar")
     public ModelAndView cadastrarEndereco(@Valid @ModelAttribute("endereco") Endereco endereco,
                                           BindingResult result, Model model,
+                                          @RequestParam(value = "redirect", required = false) String redirect,
                                           RedirectAttributes redirectAttributes) {
         if (result.hasErrors()) {
             return new ModelAndView("registrar-endereco", "endereco", endereco);
@@ -92,11 +94,16 @@ public class EnderecoController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Cliente cliente = clienteService.findByEmail(authentication.getName());
 
+        redirect = (String) model.getAttribute("redirect");
+
         endereco.setCliente(cliente);
         enderecoRepository.save(endereco);
 
         redirectAttributes.addFlashAttribute("mensagem", "Endereço cadastrado com sucesso!");
 
+        if (redirect != null && !redirect.isEmpty()) {
+            return new ModelAndView("redirect:" + redirect);
+        }
         return new ModelAndView("redirect:/endereco/lista-enderecos");
 
     }
